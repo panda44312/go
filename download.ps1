b051f0b622468319961fa292c089ee2107d67655
panda44312 <panda44312@gmali.com>
2025-02-01 17:27:06 +0100

    dddddddddddddddd

--- download.ps1
+++ download.ps1
@@ -238,7 +238,7 @@ function Show-MainMenu {
     $randomPoem = $poetry | Get-Random
     Write-Host $randomPoem
 
-    Write-Host "`n🏠 主菜单：" -ForegroundColor Cyan
+    Write-Host "`n🏠 主菜单" -ForegroundColor Cyan
     Write-Host "1. 下载单个视频"
     Write-Host "2. 下载多个视频"
     Write-Host "3. 下载播放列表"
