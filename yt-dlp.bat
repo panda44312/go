f9ad569d4a628f7433049da1e06a9ce3b66fe530
panda44312 <panda44312@gmali.com>
2025-02-01 16:53:09 +0100

    aaaaaaa

--- yt-dlp.bat
+++ yt-dlp.bat
@@ -10,11 +10,6 @@ if exist "%~dp0download.ps1" (
     del "%~dp0download.ps1"
 )
 
-:: 删除旧的 yt-dlp.bat 文件（如果存在）
-if exist "%~dp0yt-dlp.bat" (
-    del "%~dp0yt-dlp.bat"
-)
-
 :: 显示载入提示并下载最新的 PowerShell 脚本
 echo 载入中...
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/panda44312/go/main/download.ps1', '%~dp0download.ps1')"
